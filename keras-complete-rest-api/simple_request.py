5b12bee73ceb3e68c0c623c0368021d3f8f48246
imnotkind <billy307@daum.net>
2018-07-22 05:43:02 +0900

    Update simple_request.py

--- keras-complete-rest-api/simple_request.py
+++ keras-complete-rest-api/simple_request.py
@@ -6,7 +6,7 @@ import requests
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
-KERAS_REST_API_URL = "http://localhost:5000/predict"
+KERAS_REST_API_URL = "http://localhost:5522/predict"
 IMAGE_PATH = "jemma.png"
 
 # load the input image and construct the payload for the request
